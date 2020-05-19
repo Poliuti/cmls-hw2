b764366c55398c9decb4364d84aa5d7755077c6b
Matteo Bernardini <mat_teobe@yahoo.it>
2020-05-19 16:48:19 +0200

    attach label to slider + tidying up

--- project/Source/PluginEditor.cpp
+++ project/Source/PluginEditor.cpp
@@ -1,8 +1,8 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-struct UIelements {
-    String label;
+struct UISliders {
+    String label, suffix;
     float range[2];
     float (FlangerProcessor::*get_func)(void);
     void (FlangerProcessor::*set_func)(float);
@@ -16,28 +16,28 @@ FlangerEditor::FlangerEditor(FlangerProcessor& p)
     // editor's size to whatever you need it to be.
 
     // Describe GUI
-    UIelements ui_elements[] = {
-        {"LFO frequency", {0.0, 10.0},  &FlangerProcessor::get_freqOsc,    &FlangerProcessor::set_freqOsc},
-        {"Sweep width",   {0.0, 25e-3}, &FlangerProcessor::get_sweepWidth, &FlangerProcessor::set_sweepWidth},
-        {"Depth",         {0.0, 1.0},   &FlangerProcessor::get_depth,      &FlangerProcessor::set_depth},
-        {"Feedback",      {0.0, 1.0},   &FlangerProcessor::get_fb,         &FlangerProcessor::set_fb},
+    UISliders ui_elements[] = {
+        {"LFO frequency", " Hz", {0.0, 10.0},  &FlangerProcessor::get_freqOsc,    &FlangerProcessor::set_freqOsc},
+        {"Sweep width",   " s",  {0.0, 25e-3}, &FlangerProcessor::get_sweepWidth, &FlangerProcessor::set_sweepWidth},
+        {"Depth",         " %",  {0.0, 1.0},   &FlangerProcessor::get_depth,      &FlangerProcessor::set_depth},
+        {"Feedback",      " %",  {0.0, 1.0},   &FlangerProcessor::get_fb,         &FlangerProcessor::set_fb},
     };
 
-    for (UIelements item : ui_elements) {
+    for (UISliders item : ui_elements) {
         Slider* s = new Slider();
-        Label* l = new Label();
+        addAndMakeVisible(s);
+        sliders.add(s);
         s->setRange(item.range[0], item.range[1]);
         s->setValue((processor.*(item.get_func))());
-        s->setTextBoxStyle(Slider::TextBoxRight, false, 100, 20); // cambiare numeri
-        s->onValueChange = [this, s, &item] {
-            auto v = s->getValue();
-            (processor.*(item.set_func))(0.0f);
-        };
-        l->setText(item.label, dontSendNotification);
-        sliders.add(s);
-        labels.add(l);
-        addAndMakeVisible(s);
+        s->setTextValueSuffix(item.suffix);
+        s->setSliderStyle(Slider::SliderStyle::LinearHorizontal);
+        //s->setTextBoxStyle(Slider::TextBoxRight, false, 100, 20); // cambiare numeri
+        s->onValueChange = [this, s, &item] { (processor.*(item.set_func))((float)s->getValue()); };
+
+        Label* l = new Label();
         addAndMakeVisible(l);
+        l->setText(item.label, dontSendNotification);
+        l->attachToComponent(s, true);
     }
 
     setSize(400, 300);
@@ -61,12 +61,13 @@ void FlangerEditor::resized()
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
     FlexBox flow;
+    flow.flexDirection = FlexBox::Direction::column;
     flow.flexWrap = FlexBox::Wrap::wrap;
     flow.justifyContent = FlexBox::JustifyContent::center;
     flow.alignContent = FlexBox::AlignContent::center;
 
     for (Slider* s : sliders) {
-        flow.items.add(FlexItem(*s).withMinWidth(50.0f).withMinHeight(50.0f));
+        flow.items.add(FlexItem(*s).withFlex(1, 0, getHeight() / sliders.size()));
     }
 
     flow.performLayout(getLocalBounds().toFloat());
